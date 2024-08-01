09132e62d28105f5bbe5aabd2a457ac5cb063d7b
Atefeh Bakefayat <a.bakefayat@sk.local>
2024-08-01 14:06:19 +0330

    fix sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIcon } from '@angular/material/icon';
 import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatIcon, MatButton,MatIconButton],
+  imports: [RouterOutlet, MatToolbarModule, MatIcon, MatButton,MatIconButton,MatSidenavContainer,MatSidenav,MatSidenavContent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
